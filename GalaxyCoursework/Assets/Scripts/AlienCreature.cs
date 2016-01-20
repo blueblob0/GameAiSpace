a032163c37152ab8a372cc3435a36701ae00ed9e
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-01-20 14:54:27 +0000

    Head spawning working

--- GalaxyCoursework/Assets/Scripts/AlienCreature.cs
+++ GalaxyCoursework/Assets/Scripts/AlienCreature.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /* The super class that will handle all the characteristics
  * of the alien creatures that will spawn, all the random spawning variables
@@ -8,15 +9,12 @@ using System.Collections;
 
 public class AlienCreature : MonoBehaviour {
 
-    //Reference to the creature's body
-    private GameObject body;
-
     /// <summary>
     /// Will spawn the creature into the game world
     /// </summary>
     protected virtual void Start () {
-        body = createBody();
-        body.transform.SetParent(transform);
+        //Create the body of the creature
+        createBody(5);
 	}
 	
 	/// <summary>
@@ -27,19 +25,46 @@ public class AlienCreature : MonoBehaviour {
 	}
 
     /// <summary>
-    /// Spawns a body for the creature 
+    /// Spawns a body for the creature that is parented to this game object
     /// </summary>
     /// <param name="maxHeads">The maximum amount of hezads the creature can have</param>
     /// <param name="maxArms">The maximum amount of arms the creature can have</param>
     /// <param name="maxLegs">The maximum amount of legs the creature can have</param>
-    /// <returns>Returns a reference to the body created</returns>
-    protected GameObject createBody(ushort maxHeads = 1, ushort maxArms = 2, ushort maxLegs = 2){
-        GameObject returnObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-
+    protected void createBody(ushort maxHeads = 1, ushort maxArms = 2, ushort maxLegs = 2){
+        //Create a cube for the body
+        GameObject body = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //Set the position of the body the gameObject's
+        body.transform.position = transform.position;
         //Set the name
-        returnObject.name = "Body";
+        body.name = "Body";
 
-        //Return the new body
-        return returnObject;
+        //Create the heads
+        ushort headCount = (ushort)Random.Range(1, maxHeads + 1);
+        //Get the position where the heads will spawn
+        Vector3 headSpawnPos = body.transform.position;
+        headSpawnPos.y += body.GetComponent<Renderer>().bounds.size.y;
+        headSpawnPos.x += body.GetComponent<Renderer>().bounds.size.x * (headCount / 2);
+        //List to hold the heads
+        List<GameObject> heads = new List<GameObject>();
+        //Loop through the amount of heads and spawn them in
+        for(ushort i = 0; i < headCount; i++) {
+            //Spawn in the primitive
+            GameObject headToSpawn = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            //Set the parent
+            headToSpawn.transform.SetParent(body.transform);
+            //Set the position
+            headToSpawn.transform.position = headSpawnPos;
+            headSpawnPos.x -= body.GetComponent<Renderer>().bounds.size.x;
+            //Scale the heads down
+            headToSpawn.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+            //Change the name
+            headToSpawn.name = "Head " + (i + 1);
+            //Add into the list
+            heads.Add(headToSpawn);
+        }
+
+        //Create the arms
+
+        //Create the legs
     }
 }
