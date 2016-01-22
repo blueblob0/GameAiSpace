32dcb84b411a2b7997845ad0eb5039052c3e261a
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-01-22 20:00:00 +0000

    Arm spawning now in + other fixes

--- GalaxyCoursework/Assets/Scripts/AlienCreature.cs
+++ GalaxyCoursework/Assets/Scripts/AlienCreature.cs
@@ -32,8 +32,16 @@ public class AlienCreature : MonoBehaviour {
             maxAmountOfLegs = 2;
         }
 
+        //Get the object's rotation
+        Quaternion rot = transform.rotation;
+        //Reset it to 0
+        transform.rotation = new Quaternion();
+
         //Create the creature
         createCreature(maxAmountOfHeads, maxAmountOfLegs, maxAmountOfLegs);
+
+        //Now the creature has been created, re apply the rotation
+        transform.rotation = rot;
 	}
 	
 	/// <summary>
@@ -57,6 +65,8 @@ public class AlienCreature : MonoBehaviour {
             body.transform.position = transform.position;
             //Set the name
             body.name = "Body";
+            //Make the body a child of this object
+            body.transform.SetParent(transform);
 
             //Create the heads
             ushort headCount = (ushort)Random.Range(1, maxHeads + 1);
@@ -67,8 +77,6 @@ public class AlienCreature : MonoBehaviour {
             GameObject headParent = new GameObject("Head Parent");
             headParent.transform.position = headSpawnPos;
             headParent.transform.SetParent(body.transform);
-            //List to kee ptrack of all the heads
-            List<GameObject> heads = new List<GameObject>();
             //Loop through the amount of heads and spawn them in
             for(ushort i = 0; i < headCount; i++) {
                 //Spawn in the primitive
@@ -82,8 +90,6 @@ public class AlienCreature : MonoBehaviour {
                 headToSpawn.transform.SetParent(headParent.transform);
                 //Change the name
                 headToSpawn.name = "Head " + (i + 1);
-                //Add to the list
-                heads.Add(headToSpawn);
             }
             //Center the heads
             if(headCount > 1) {
@@ -93,11 +99,58 @@ public class AlienCreature : MonoBehaviour {
             }
 
             //Create the arms
-
+            ushort armCount = (ushort)Random.Range(1, maxArms + 1);
+            //Get the position the arms will spawn
+            Vector3 armSpawnPos = body.transform.position;
+            armSpawnPos.x += body.GetComponent<Renderer>().bounds.size.x;
+            //Parent to hold all of the arms
+            GameObject armParent = new GameObject("Arm Parent");
+            armParent.transform.position = body.transform.position;
+            armParent.transform.SetParent(body.transform);
+            //Loop through the arm count and spawn in the arms
+            for(ushort i = 0; i < armCount; i++) {
+                //Multiply the position by to put the arms on each side
+                short spawnMod;
+                //If the number is even we want to spawn the arm on X+ else spawn on X-
+                if(isEven(i)) {
+                    spawnMod = 1;
+                } else {
+                    spawnMod = -1;
+                }
+                //Spawn in the primitive
+                GameObject armtoSpawn = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                //Set the position
+                armtoSpawn.transform.position = armSpawnPos;
+                Vector3 temp = armtoSpawn.transform.position;
+                temp.x *= spawnMod;
+                armtoSpawn.transform.position = temp;
+                //Scale the arm
+                armtoSpawn.transform.localScale = new Vector3(0.25f, 0.75f, 0.25f);
+                //rotate
+                armtoSpawn.transform.rotation = Quaternion.Euler(0, 0, 90);
+                //Set the Parent
+                armtoSpawn.transform.SetParent(armParent.transform);
+                //Change the name
+                if(isEven(i)) {
+                    armtoSpawn.name = "Right Arm " + (i + 1);
+                } else {
+                    armtoSpawn.name = "Left Arm " + (i + 1);
+                }
+            }
+            
             //Create the legs
 
             //Creature has now been spawned
             spawned = true;
         }
+    }
+
+    /// <summary>
+    /// Returns true if the value passed is even
+    /// </summary>
+    /// <param name="value">Value to pass</param>
+    /// <returns></returns>
+    bool isEven(int value) {
+        return value % 2 == 0;
     }
 }
