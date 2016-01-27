9a90cfc228b7da2a3d833f482c980e847d81ab76
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-01-27 11:37:33 +0000

    Made the arms move back

--- GalaxyCoursework/Assets/Scripts/AlienCreature.cs
+++ GalaxyCoursework/Assets/Scripts/AlienCreature.cs
@@ -21,7 +21,7 @@ public class AlienCreature : MonoBehaviour {
     /// <summary>
     /// Will spawn the creature into the game world
     /// </summary>
-    protected virtual void Start () {
+    protected virtual void Start() {
         //Check that nthoing is out of bounds
         if(maxAmountOfHeads < 1) {
             maxAmountOfHeads = 1;
@@ -46,14 +46,14 @@ public class AlienCreature : MonoBehaviour {
 
         //Now the creature has been created, re apply the rotation
         transform.rotation = rot;
-	}
-	
-	/// <summary>
+    }
+
+    /// <summary>
     /// Update the creature to peform movement ect.
     /// </summary>
-	protected virtual void Update () {
-	
-	}
+    protected virtual void Update() {
+
+    }
 
     /// <summary>
     /// Spawns in the creature
@@ -61,7 +61,7 @@ public class AlienCreature : MonoBehaviour {
     /// <param name="maxHeads">The maximum amount of hezads the creature can have</param>
     /// <param name="maxArms">The maximum amount of arms the creature can have</param>
     /// <param name="maxLegs">The maximum amount of legs the creature can have</param>
-    protected void createCreature(ushort maxHeads = 1, ushort maxArms = 2, ushort maxLegs = 2){
+    protected void createCreature(ushort maxHeads = 1, ushort maxArms = 2, ushort maxLegs = 2) {
         if(!spawned) {
             //Get the random values so we know what dimentions to make the body
             ushort headCount = (ushort)Random.Range(1, maxHeads + 1);
@@ -118,40 +118,54 @@ public class AlienCreature : MonoBehaviour {
             armParent.transform.SetParent(body.transform);
             //Used to position the arms
             int twoCheck = 2;
+            //Keep track of how many arms are placed
+            ushort totalArmsPlaced = 0;
             //Loop through the arm count and spawn in the arms
-            for(ushort i = 0; i < armCount; i++) {
-                //Multiply the position by to put the arms on each side
-                short spawnMod;
-                //If the number is even we want to spawn the arm on X+ else spawn on X-
-                if(isEven(i)) {
-                    spawnMod = 1;
-                } else {
-                    spawnMod = -1;
+            for(int i = 0; i < armCount; i += 2) {
+                for(int j = 0; j < 4; j++) {
+                    if(totalArmsPlaced < armCount) {
+                        //Multiply the position by to put the arms on each side
+                        short spawnMod;
+                        //If the number is even we want to spawn the arm on X+ else spawn on X-
+                        if(isEven(j)) {
+                            spawnMod = 1;
+                        } else {
+                            spawnMod = -1;
+                        }
+                        //Spawn in the primitive
+                        GameObject armtoSpawn = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                        //Set the position
+                        armtoSpawn.transform.position = armSpawnPos;
+                        Vector3 temp = armtoSpawn.transform.position;
+                        temp.x *= spawnMod;
+                        armtoSpawn.transform.position = temp;
+                        if(j == twoCheck - 1) {
+                            armSpawnPos.y -= body.GetComponent<Renderer>().bounds.size.x / 2;
+                            twoCheck += 2;
+                        }
+                        //Scale the arm
+                        armtoSpawn.transform.localScale = new Vector3(0.25f, 0.75f, 0.25f);
+                        //rotate
+                        armtoSpawn.transform.rotation = Quaternion.Euler(0, 0, 90);
+                        //Set the Parent
+                        armtoSpawn.transform.SetParent(armParent.transform);
+                        //Change the name
+                        if(isEven(j)) {
+                            armtoSpawn.name = "Arm " + (i + 1) + " R";
+                        } else {
+                            armtoSpawn.name = "Arm " + (i + 1) + " L";
+                        }
+                        //Increase the count
+                        totalArmsPlaced++;
+                    }
                 }
-                //Spawn in the primitive
-                GameObject armtoSpawn = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-                //Set the position
-                armtoSpawn.transform.position = armSpawnPos;
-                Vector3 temp = armtoSpawn.transform.position;
-                temp.x *= spawnMod;
-                armtoSpawn.transform.position = temp;
-                if(i == twoCheck - 1) {
-                    armSpawnPos.y -= armtoSpawn.GetComponent<Renderer>().bounds.size.x / 2;
-                    twoCheck += 2;
-                }
-                //Scale the arm
-                armtoSpawn.transform.localScale = new Vector3(0.25f, 0.75f, 0.25f);
-                //rotate
-                armtoSpawn.transform.rotation = Quaternion.Euler(0, 0, 90);
-                //Set the Parent
-                armtoSpawn.transform.SetParent(armParent.transform);
-                //Change the name
-                if(isEven(i)) {
-                    armtoSpawn.name = "Arm " + (i + 1) + " R";
-                } else {
-                    armtoSpawn.name = "Arm " + (i + 1) + " L";
-                }
+                //Reset the position
+                armSpawnPos.y += body.GetComponent<Renderer>().bounds.size.x;
+                armSpawnPos.z -= body.GetComponent<Renderer>().bounds.size.z;
+                //Put this back to 2
+                twoCheck = 2;
             }
+
 
             //------------------Create the legs----------------------\\
             //Get the position the legs will spawn
@@ -168,7 +182,7 @@ public class AlienCreature : MonoBehaviour {
             //Legs get placed in rows of 2
             for(int i = 0; i < legCount; i += 2) { //first loop does the group of legs (i.e front two or back two)
                 for(int j = 0; j < 2; j++) { //Second loop does the actual legs
-                    //Make sure we dont spawn extra legs
+                                             //Make sure we dont spawn extra legs
                     if(totalLegsPlaced < legCount) {
                         //Spawn in the primitve
                         GameObject legToSpawn = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
@@ -181,6 +195,8 @@ public class AlienCreature : MonoBehaviour {
                         legToSpawn.transform.SetParent(legParent.transform);
                         //Change the name
                         legToSpawn.name = "Leg " + (i + 1);
+                        //Increased the count
+                        totalLegsPlaced++;
                     }
                 }
                 //Move the position back
