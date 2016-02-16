2373104ab3bcb3b3131afbc2d45ec75d3c3da5d3
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-02-16 15:28:13 +0000

    Added spawning functionality

--- GalaxyCoursework/Assets/Scripts/Aliens/AlienBody.cs
+++ GalaxyCoursework/Assets/Scripts/Aliens/AlienBody.cs
@@ -33,6 +33,49 @@ public class AlienBody : MonoBehaviour{
         }
     }
 
+    //These functions rotate the limbs (for walknig ect)
+
+    //These functions add and parent a gameobject onto the available slots
+    public void addHead(GameObject headPrefab) {
+        //Loop through the available spots
+        foreach(GameObject spot in headSpots) {
+            //Check if the spot is free
+            if(spot.transform.childCount == 0) {
+                //Add and set up the new body part
+                GameObject head = GameObject.Instantiate<GameObject>(headPrefab);
+                head.transform.SetParent(spot.transform);
+                head.transform.position = spot.transform.position;
+                head.transform.rotation = spot.transform.rotation;
+            }
+        }
+    }
+    public void addArm(GameObject armPrefab) {
+        //Loop through the available spots
+        foreach(GameObject spot in armSpots) {
+            //Check if the spot is free
+            if(spot.transform.childCount == 0) {
+                //Add and set up the new body part
+                GameObject arm = GameObject.Instantiate<GameObject>(armPrefab);
+                arm.transform.SetParent(spot.transform);
+                arm.transform.position = spot.transform.position;
+                arm.transform.rotation = spot.transform.rotation;
+            }
+        }
+    }
+    public void addLeg(GameObject legPrefab) {
+        //Loop through the available spots
+        foreach(GameObject spot in legSpots) {
+            //Check if the spot is free
+            if(spot.transform.childCount == 0) {
+                //Add and set up the new body part
+                GameObject leg = GameObject.Instantiate<GameObject>(legPrefab);
+                leg.transform.SetParent(spot.transform);
+                leg.transform.position = spot.transform.position;
+                leg.transform.rotation = spot.transform.rotation;
+            }
+        }
+    }
+
     //Getters
     public int getHeadSpotCount() {
         return headSpots.Count;
