62091c5caae6e5ed25c25f4b9322fa1b4b243d9b
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-02-16 15:18:19 +0000

    Added more functionality to the alien body script

--- GalaxyCoursework/Assets/Scripts/Aliens/AlienBody.cs
+++ GalaxyCoursework/Assets/Scripts/Aliens/AlienBody.cs
@@ -1,12 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /*
  * Simple class that just holds some basic info on the body
  */
 
 public class AlienBody : MonoBehaviour{
-    public ushort headSpots;
-    public ushort armSpots;
-    public ushort legSpots;
+    private List<GameObject> headSpots;
+    private List<GameObject> armSpots;
+    private List<GameObject> legSpots;
+
+    void Start() {
+        headSpots = new List<GameObject>();
+        armSpots = new List<GameObject>();
+        legSpots = new List<GameObject>();
+
+        //Get all the head/arm/leg spots on start
+        for(int i = 0; i < transform.childCount; i++) {
+            //Get each child
+            Transform child = transform.GetChild(i);
+            //Then check their name to see what 'spot' they are
+            if(child.name[0] == 'H') {
+                headSpots.Add(child.gameObject);
+            }
+            if(child.name[0] == 'A') {
+                armSpots.Add(child.gameObject);
+            }
+            if(child.name[0] == 'L') {
+                legSpots.Add(child.gameObject);
+            }
+        }
+    }
 }
