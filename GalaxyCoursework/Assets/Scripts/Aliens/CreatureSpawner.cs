6fc1ba298fcd8eacadf3183272fbb87145f08f04
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-02-24 13:11:50 +0000

    Added a creature spawner class

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/CreatureSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatureSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
