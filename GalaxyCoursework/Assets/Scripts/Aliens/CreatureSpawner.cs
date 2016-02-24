4899df408fda4c593dd3ca69875f1c5a70702841
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-02-24 13:16:16 +0000

    Added comment and array of spawnable creatures

--- GalaxyCoursework/Assets/Scripts/Aliens/CreatureSpawner.cs
+++ GalaxyCoursework/Assets/Scripts/Aliens/CreatureSpawner.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * This script is attached to planets and will spawn a certain amount of creature types
+ */
+
 public class CreatureSpawner : MonoBehaviour {
+
+    //Set in inspector
+    public GameObject[] creatureTypes;
 
 	// Use this for initialization
 	void Start () {
