ae0792084ad530bb2816b278f557c5f43ce38f75
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-02-24 13:30:29 +0000

    First iteration of spawning on planets

--- GalaxyCoursework/Assets/Scripts/Aliens/CreatureSpawner.cs
+++ GalaxyCoursework/Assets/Scripts/Aliens/CreatureSpawner.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 /*
- * This script is attached to planets and will spawn a certain amount of creature types
+ * This script is attached to planets and will spawn a 
+ * a certain amount of creatures provided
  */
 
 public class CreatureSpawner : MonoBehaviour {
@@ -10,13 +11,23 @@ public class CreatureSpawner : MonoBehaviour {
     //Set in inspector
     public GameObject[] creatureTypes;
 
+    //Quick way of checking what creature type has been spawned
+    private bool[] creatureSpawned;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+        //Init the bool array
+        creatureSpawned = new bool[creatureTypes.Length];
+
+        //How many total types to spawn
+        int amountOfTypestoSpawn = Random.Range(0, creatureTypes.Length);
+
+        //Loop through the array and start spawning creatures
+        for(int i = 0; i < amountOfTypestoSpawn; i++) {
+            if(!creatureSpawned[i]) {
+                GameObject.Instantiate<GameObject>(creatureTypes[i]);
+                creatureSpawned[i] = true;
+            }
+        }
 	}
 }
