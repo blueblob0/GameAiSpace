7b0d2167e29e5fb5070d5da93de1d5f1e55f2a71
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-03-28 15:44:21 +0100

    Added seek alg

--- GalaxyCoursework/Assets/Scripts/Aliens/AlienAI.cs
+++ GalaxyCoursework/Assets/Scripts/Aliens/AlienAI.cs
@@ -9,6 +9,9 @@ public class AlienAI : MonoBehaviour {
 
     //Set in inspector
     public ushort speed = 1;
+
+    //The velocity vector
+    private Vector3 velocity;
    
     // Use this for initialization
     public virtual void Start () {
@@ -17,11 +20,17 @@ public class AlienAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	public virtual void Update () {
-	
+        //Update the agent's position based on the velcoity
+        transform.position += (velocity * speed * Time.deltaTime);
+
+
 	}
 
-    protected Vector3 seek(Vector3 targetWorldPos) {
-        return Vector3.zero;
+    protected Vector3 seek(Vector3 currentPos, Vector3 targetWorldPos) {
+        Vector3 ret = targetWorldPos - currentPos;
+        ret.Normalize();
+
+        return ret;
     }
 
     protected Vector3 flee(Vector3 targetWorldPos) {
