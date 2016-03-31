0b5700669f39ee60cb6057ce273983eec0c6dfee
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-03-31 16:29:20 +0100

    Attempting to get steering working - keeps bugging out

--- GalaxyCoursework/Assets/Scripts/Aliens/AlienAI.cs
+++ GalaxyCoursework/Assets/Scripts/Aliens/AlienAI.cs
@@ -9,35 +9,83 @@ public class AlienAI : MonoBehaviour {
 
     //Set in inspector
     public ushort speed = 1;
+    public ushort steeringForce = 5;
+
+    //The target of the agent
+    private GameObject target;
 
     //The velocity vector
     private Vector3 velocity;
+    //Used to apply steering
+    private Vector3 steering;
+    private Vector3 desiredVelocity;
+
+    //DEBUG
+    private GameObject testCube;
+    private float wait = 3;
+    private float then = 0;
 
     // Use this for initialization
     public virtual void Start () {
-        velocity = Vector3.zero;
-	}
+        velocity = transform.forward * speed * Time.deltaTime;
+        steering = Vector3.zero;
+        desiredVelocity = Vector3.zero;
+
+        testCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        testCube.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+    }
 	
 	// Update is called once per frame
 	public virtual void Update () {
-        //Get the desired velocity and calculate the steering
-        velocity = wander(transform.position, transform.forward, 2);
+        //DEBUG---------------------
+        if(then >= wait) {
+            testCube.transform.position = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
 
-        //Update the agent's position and rotation based on the velocity
+            target = testCube;
+
+            then = 0;
+        } else {
+            then += Time.deltaTime;
+        }
+        //--------------------------
+
+        displayDebugLines();
+
+        //Get a steering force
+        if(target) {
+            steering = seek(target.transform.position);
+        }
+
+        //Add to the velocity
+        velocity += steering;
+
+        //Normalise the desired velocity and add the speed
         velocity.Normalize();
         velocity *= speed * Time.deltaTime;
-        transform.rotation = Quaternion.LookRotation(velocity);
+
+        //Update the position
         transform.position += velocity;
     }
 
     /// <summary>
     /// Makes the agent seek the target
     /// </summary>
-    /// <param name="currentPos">Current position of the agent</param>
     /// <param name="targetWorldPos">The world position of the target to seek</param>
-    /// <returns></returns>
-    protected Vector3 seek(Vector3 currentPos, Vector3 targetWorldPos) {
-        return targetWorldPos - currentPos;
+    /// <returns>Returns the steering force</returns>
+    protected Vector3 seek(Vector3 targetWorldPos) {
+        //The steering force
+        Vector3 force;
+
+        //Set the diesried velocity to the direction of the target
+        desiredVelocity = targetWorldPos - transform.position;
+        //Scale to the speed of the agent
+        desiredVelocity.Normalize();
+        desiredVelocity *= speed * Time.deltaTime;
+
+        //Return the steering force of the desired velocity
+        force = desiredVelocity - velocity;
+        return force;
+
     }
 
     /// <summary>
@@ -73,5 +121,17 @@ public class AlienAI : MonoBehaviour {
 
         //Return the new wander force
         return circleCenter + displacement;
+    }
+
+    /// <summary>
+    /// Displays the debug lines for the velocity(Green), steering(Red) and desired velocity(Blue)
+    /// </summary>
+    private void displayDebugLines() {
+        //Desired velocity
+        Debug.DrawLine(transform.position, transform.position + (desiredVelocity.normalized * 5), Color.blue);
+        //Steering
+        Debug.DrawLine(transform.position, transform.position + (steering.normalized * 5), Color.red);
+        //Velocity
+        Debug.DrawLine(transform.position, transform.position + (velocity.normalized * 5), Color.green);
     }
 }
