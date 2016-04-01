2826399ba0e26bda9f407e4564079e40b3ffa743
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-01 14:38:03 +0100

    Correctly displays debuglines

--- GalaxyCoursework/Assets/Scripts/Aliens/AlienAI.cs
+++ GalaxyCoursework/Assets/Scripts/Aliens/AlienAI.cs
@@ -165,7 +165,7 @@ public class AlienAI : MonoBehaviour {
         //Desired velocity
         Debug.DrawLine(transform.position, transform.position + (desiredVelocity.normalized * 5), Color.blue);
         //Steering
-        Debug.DrawLine(transform.position + (desiredVelocity.normalized * 5), transform.position + (desiredVelocity.normalized * 5) + (steering.normalized * 5), Color.red);
+        Debug.DrawLine(transform.position + (velocity.normalized * 5), transform.position + (velocity.normalized * 5) + (steering.normalized * 2), Color.red);
         //Velocity
         Debug.DrawLine(transform.position, transform.position + (velocity.normalized * 5), Color.green);
     }
