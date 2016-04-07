026071a512dade72399a0b2b1515f08afce72933
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-07 22:25:39 +0100

    Added new creature types

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/AgressiveAlien.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * An alien creature with agressive characteristics
+ */
+public class AgressiveAlien : AlienCreature {
+
+    //Empty for now
+
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+	
+	//}
+}
