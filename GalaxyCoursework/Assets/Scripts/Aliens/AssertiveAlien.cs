ed9f3dc90bd4a364c6932f8a4e850b77381051b2
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-08 11:14:40 +0100

    Added an assertive creature type

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/AssertiveAlien.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * An Alien creature with assertvie characteristics
+ *
+ * The agent will sometimes seek out targets to attack
+ * The agent will fight back or flee
+ */
+public class AssertiveAlien : AlienCreature {
+
+    //Empty for now
+
+	//// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+	
+	//}
+}
