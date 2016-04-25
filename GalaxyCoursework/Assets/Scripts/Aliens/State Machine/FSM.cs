9e9cc45b8af382c888c6ca5b4e2257cf87da4ac3
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-25 15:52:21 +0100

    Moved

--- GalaxyCoursework/Assets/Scripts/Aliens/State Machine/FSM.cs
+++ GalaxyCoursework/Assets/Scripts/Aliens/State Machine/FSM.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//The class that will be the Finite State Machine the aliens use
 public class FSM : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	    //Empty for now
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    //Empty for now
 	}
 }
