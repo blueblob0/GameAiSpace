f10577309a490cc3cf73e167244b5ee70c11ed73
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-28 15:32:46 +0100

    Added in sight task

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/EnemyInSight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/*
+ * The task to check if an enemy is in sight
+ */
+public class EnemyInSight : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public EnemyInSight(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        //Will be true if there are Enemies near
+        return agentRef.getNearTargets().Count > 0;
+    }
+}
