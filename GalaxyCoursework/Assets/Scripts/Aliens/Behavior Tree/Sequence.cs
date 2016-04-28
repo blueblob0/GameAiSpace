9d753223864890770025f43ed9d01cf94b95b24d
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-28 14:52:25 +0100

    Added the sequence node class

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Sequence.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ * Sequencer for the behavior tree
+ */
+public class Sequence : Task {
+    //The children this sequencer activates
+    private List<Task> children;
+
+    public bool activate() {
+        //Loop through each child node and activate them
+        foreach(Task child in children) {
+            if(!child.activate()) {
+                return false;
+            }
+        }
+        //Return true when all children are sucessful
+        return true;
+    }
+
+    /// <summary>
+    /// Adds a child onto this node
+    /// </summary>
+    /// <param name="child">Child to add</param>
+    public void addChild(Task child) {
+        children.Add(child);
+    }
+}
