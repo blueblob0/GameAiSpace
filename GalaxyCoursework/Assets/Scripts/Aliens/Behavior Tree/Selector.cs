7a5bfd23ccc3f0369d4b2868a69918826cbe9d80
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-28 14:54:44 +0100

    Added selector node

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Selector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ * Selector for the behavior tree
+ */
+public class Selector : Task {
+    //The children this sequencer activates
+    private List<Task> children;
+
+    public bool activate() {
+        //Loop through each child node and activate them
+        foreach(Task child in children) {
+            if(child.activate()) {
+                return true;
+            }
+        }
+        //Return false if it did not select an action
+        return false;
+    }
+
+    /// <summary>
+    /// Adds a child onto this node
+    /// </summary>
+    /// <param name="child">Child to add</param>
+    public void addChild(Task child) {
+        children.Add(child);
+    }
+}
