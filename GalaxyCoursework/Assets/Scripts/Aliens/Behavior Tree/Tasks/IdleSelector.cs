90b64aa2a7d5b64714fe9ec17f4f682296b42422
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-28 20:09:38 +0100

    Added the Idle state selector

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/IdleSelector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * The selector to control the Idle behavior
+ */
+public class IdleSelector : Selector {
+
+    //The tasks for this behavior
+    FlockSequence flock;
+    Wander wander;
+
+    public IdleSelector(AlienAI agent) : base(agent) {
+        //Construct tasks
+        flock = new FlockSequence(agent);
+        wander = new Wander(agent);
+
+        //Add in the tasks, order is important
+        addChild(flock);
+        addChild(wander);
+    }
+}
