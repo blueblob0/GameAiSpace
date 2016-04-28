4d11420c8a940326f7d7ff657011f682440dc87d
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-28 15:58:20 +0100

    Added escape sequence

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/EscapeSequence.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeSequence : Sequence {
+
+    //constrcutor
+    public EscapeSequence(AlienAI agent) : base(agent) {
+        //empty for now
+    }
+	
+}
