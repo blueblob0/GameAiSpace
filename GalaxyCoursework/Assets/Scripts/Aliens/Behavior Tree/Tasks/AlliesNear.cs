5933d658fc3a98239ea069252f4fdbf787d6cf9b
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-28 19:46:13 +0100

    Added allies near

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/AlliesNear.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/*
+ * The task to check if allies are near
+ */
+public class AlliesNear : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public AlliesNear(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        //returns true if there are other creatures near
+        return agentRef.getOtherCreatures().Count > 0;
+    }
+}
