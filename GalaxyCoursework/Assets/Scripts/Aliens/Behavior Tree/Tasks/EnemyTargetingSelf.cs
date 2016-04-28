290298a50e49aebab9bd4f7a91b3d5b2c023d4e0
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-28 15:45:21 +0100

    Added targetting self task

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/EnemyTargetingSelf.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EnemyTargetingSelf : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public EnemyTargetingSelf(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        //Loop through near targets
+        foreach(AlienCreature target in agentRef.getNearTargets()) {
+            //If any of the near targets are targetting this agent
+            if(target.getTarget() != null && target.getTarget() == agentRef) {
+                //Set the target and return true
+                agentRef.setTarget(target);
+                return true;
+            }
+        }
+        //False if none are targetting this
+        return false;
+    }
+}
