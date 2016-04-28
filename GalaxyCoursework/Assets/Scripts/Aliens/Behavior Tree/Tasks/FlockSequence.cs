05bbad3cc3eded82fadd421767ee5dbbb410b00d
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-28 20:07:00 +0100

    Added flocknig sequence

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/FlockSequence.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * The sequence to control the flocking of the agent
+ */
+public class FlockSequence : Sequence {
+
+    //The tasks that make this sequence
+    AlliesNear near;
+    Flock flock;
+
+    //constructor
+    public FlockSequence(AlienAI agent) : base(agent) {
+        //Construct tasks
+        near = new AlliesNear(agent);
+        flock = new Flock(agent);
+
+        //Add the tasks to the list of children, order is important
+        addChild(near);
+        addChild(flock);
+    }
+}
