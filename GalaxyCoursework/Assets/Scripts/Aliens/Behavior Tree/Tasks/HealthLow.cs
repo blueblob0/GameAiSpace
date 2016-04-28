fd2a7c43039e6a102f8b0309f441e120f6572a66
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-28 15:47:22 +0100

    Added health low task

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/HealthLow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HealthLow : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public HealthLow(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        //True if health is below 30%
+        return agentRef.getHealth() <= agentRef.getHealth() * 0.3;
+    }
+}
