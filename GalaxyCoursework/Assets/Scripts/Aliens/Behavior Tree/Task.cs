e36b0b138b5148c05016e814f8e3fd0a8ebe9300
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-28 14:46:50 +0100

    Added the task class

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Task.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Interface class for each task, sequence and selector of the behavior tree
+ */
+public interface Task {
+
+    /// <summary>
+    /// Called to activate this task
+    /// </summary>
+    /// <returns></returns>
+    bool activate();
+}
