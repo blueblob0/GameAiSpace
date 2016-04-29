7ece5547ee296830bdd5a539177b07218af0f949
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 15:00:58 +0100

    Added energy low task

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/EnergyLow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Task to check if the agent's energy is low
+ */
+public class EnergyLow : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public EnergyLow(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        //True if energy is below 25%
+        return agentRef.getEnergy() <= agentRef.getMaxEnergy() * 0.25;
+    }
+}
