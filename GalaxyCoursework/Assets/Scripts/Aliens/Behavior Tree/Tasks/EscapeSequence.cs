39d0eca272af1a79ecf2213319d727386dba3a30
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 10:59:06 +0100

    Reordered

--- GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/EscapeSequence.cs
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/EscapeSequence.cs
@@ -3,23 +3,23 @@ using System.Collections;
 
 public class EscapeSequence : Sequence {
 
+    private HealthLow HPlow;
     private EnemyInSight sight;
     private EnemyTargetingSelf targettingSelf;
-    private HealthLow HPlow;
     private Evade evade;
 
     //constrcutor
     public EscapeSequence(AlienAI agent) : base(agent) {
         //Construct the tasks
+        HPlow = new HealthLow(agent);
         sight = new EnemyInSight(agent);
         targettingSelf = new EnemyTargetingSelf(agent);
-        HPlow = new HealthLow(agent);
         evade =  new Evade(agent);
 
         //Add the tasks to the list of children, order is important
+        addChild(HPlow);
         addChild(sight);
         addChild(targettingSelf);
-        addChild(HPlow);
         addChild(evade);
     }
 }
