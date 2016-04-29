ff3649b7a9d7b45929eb18a7d069190571c9c672
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 11:28:13 +0100

    Added the sequence to get the target

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/GetTargetSequence.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Sequence to geta target
+ */
+public class GetTargetSequence : Sequence {
+    //Tasks for this sequence
+    private EnemyInSight inSight;
+    private GetTarget getTarget;
+
+    //constructor
+    public GetTargetSequence(AlienAI agent) : base (agent) {
+        //Construct the tasks
+        inSight = new EnemyInSight(agent);
+        getTarget = new GetTarget(agent);
+
+        //Add the children, order is important
+        addChild(inSight);
+        addChild(getTarget);
+    }
+}
