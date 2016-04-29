a79d4cf0fbcafb9c3e625ced12e7f6395b10c3cf
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 15:04:01 +0100

    Added rest task

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/Rest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/*
+ * The task to replish energy
+ */
+public class Rest : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public Rest(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        //Stop movement
+        agentRef.setTargetSpeed(0);
+        //Increase energy
+        agentRef.increaseEnergy(0.5f);
+        return true;
+    }
+}
