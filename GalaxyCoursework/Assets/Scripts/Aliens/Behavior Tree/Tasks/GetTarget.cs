315f05aa40599addd32754fcf4d33b8255214b4b
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 11:16:57 +0100

    Added task to get a target

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/GetTarget.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * The task to get a target for the agent
+ */
+public class GetTarget : MonoBehaviour {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public GetTarget(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        //Loop through and get the first target
+        //TODO get a target bassed off of the 'shouldAttack' function
+        foreach(AlienAI alien in agentRef.getNearTargets()) {
+            agentRef.setTarget(alien);
+            break; //Break for now
+        }
+        return true;
+    }
+}
