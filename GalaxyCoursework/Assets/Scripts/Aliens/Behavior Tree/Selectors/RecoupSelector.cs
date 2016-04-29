f6ca38d3b411914be6af075b02501f6e11c25ba7
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 15:22:33 +0100

    Added selector for recouperation

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Selectors/RecoupSelector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Selector for recouperating the agent
+ */
+public class RecoupSelector : Selector {
+    //Tasks for this selector
+    private RestSequence rest;
+    private HealSequence heal;
+
+    //constructor
+    public RecoupSelector(AlienAI agent) : base(agent) {
+        //Construct the tasks
+        rest = new RestSequence(agent);
+        heal = new HealSequence(agent);
+
+        //Add the children, order is not as important
+        addChild(rest);
+        addChild(heal);
+    }
+}
