05dc707d479a92e226538ac4b361f0ea5bf82876
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 11:23:16 +0100

    Added attack sequencer

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/AttackSequence.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * The sequence to attack a target
+ */
+public class AttackSequence : Sequence {
+    //Tasks for this sequence
+    private HaveTarget haveTarget;
+    private EnemyInRange inRange;
+    private Attack attack;
+
+    //constructor
+    public AttackSequence(AlienAI agent) : base (agent) {
+        //Construct the tasks
+        haveTarget = new HaveTarget(agent);
+        inRange = new EnemyInRange(agent);
+        attack = new Attack(agent);
+
+        //Add the children, order is important
+        addChild(haveTarget);
+        addChild(inRange);
+        addChild(attack);
+    }
+}
