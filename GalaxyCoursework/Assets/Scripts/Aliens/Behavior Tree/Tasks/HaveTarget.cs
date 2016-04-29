648ed162e88dab04323f286a95ca12fd5be519b0
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 10:58:09 +0100

    Added task for having a target

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/HaveTarget.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/*
+ * The task to check if the agent has a target
+ */
+public class HaveTarget : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public HaveTarget(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        //True if the target is not null
+        return agentRef.getTarget() != null;
+    }
+}
