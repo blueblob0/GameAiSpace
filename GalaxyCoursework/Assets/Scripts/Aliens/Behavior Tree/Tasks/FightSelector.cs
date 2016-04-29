480161eae4bf1ca9465b8e58cc0f1a60c47765fe
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 11:31:24 +0100

    Added fight slecector

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/FightSelector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Selector for the fight behavior
+ */
+public class FightSelector : Selector {
+    //Tasks for this selector
+    private AttackSequence attackSq;
+    private PursueSequence pursueSq;
+    private GetTargetSequence getTargSq;
+
+    //constructor
+    public FightSelector(AlienAI agent) : base (agent) {
+        //Construct the tasks
+        attackSq = new AttackSequence(agent);
+        pursueSq = new PursueSequence(agent);
+        getTargSq = new GetTargetSequence(agent);
+
+        //Add the children, order is important
+        addChild(attackSq);
+        addChild(pursueSq);
+        addChild(getTargSq);
+    }
+}
