442c9bdfbbe8eba1c55ab7da9ec6167540ade5b9
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 15:15:13 +0100

    Added sequencer for resting

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Sequences/RestSequence.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * The sequence to make the agent rest
+ */
+public class RestSequence : Sequence {
+    //Tasks for this sequence
+    private EnergyLow eLow;
+    private Rest rest;
+
+    //constructor
+    public RestSequence(AlienAI agent) : base(agent) {
+        //Construct the tasks
+        eLow = new EnergyLow(agent);
+        rest = new Rest(agent);
+
+        //Add the children, order is important
+        addChild(eLow);
+        addChild(rest);
+    }
+}
