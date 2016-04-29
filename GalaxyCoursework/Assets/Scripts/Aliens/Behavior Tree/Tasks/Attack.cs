f9744840a19d97e8488288535067283d95e79d59
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 11:05:28 +0100

    Added attack task

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/Attack.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Task to attack enemys
+ */
+public class Attack : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public Attack(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        //Damage the creature then return
+        agentRef.damageCreature(agentRef.getTarget());
+        return true;
+    }
+}
