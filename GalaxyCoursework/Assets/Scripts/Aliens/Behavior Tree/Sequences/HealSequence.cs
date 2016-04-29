493579f6a5e538456a186ebc9acd8142c8e016d0
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 15:17:55 +0100

    Added sequence for healing

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Sequences/HealSequence.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Sequence for healing the agent
+ */
+public class HealSequence : Sequence {
+    //Tasks for this sequence
+    private HealthLow hLow;
+    private Heal heal;
+
+    //constructor
+    public HealSequence(AlienAI agent) : base(agent) {
+        //Construct the tasks
+        hLow = new HealthLow(agent);
+        heal = new Heal(agent);
+
+        //Add the children, order is important
+        addChild(hLow);
+        addChild(heal);
+    }
+}
