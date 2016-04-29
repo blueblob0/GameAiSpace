b620810ef11aba4b43fb3404f9692137d7e64c55
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 11:25:25 +0100

    Added the pursue sequence

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/PursueSequence.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PursueSequence : Sequence {
+    //Tasks for this sequence
+    private HaveTarget haveTarget;
+    private Pursue pursue;
+
+    //constructor
+    public PursueSequence(AlienAI agent) : base (agent) {
+        //Construct the tasks
+        haveTarget = new HaveTarget(agent);
+        pursue = new Pursue(agent);
+
+        //Add the children, order is important
+        addChild(haveTarget);
+        addChild(pursue);
+    }
+}
