7cdc26e8411a429d845c5c000e18f52ce6036337
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-29 11:13:20 +0100

    Added pursue task

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/Pursue.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * The task to pursue a target
+ */
+public class Pursue : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public Pursue(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        //Pursue the target
+        pursueSteering(agentRef.getTarget());
+        return true;
+    }
+
+    /// <summary>
+    /// Makes the agent seek the target
+    /// </summary>
+    /// <param name="targetWorldPos">The world position of the target to seek</param>
+    /// <returns>Returns the steering force of the desired direction</returns>
+    protected Vector3 seek(Vector3 targetWorldPos) {
+        //Set the diesried velocity to the direction of the target
+        agentRef.setDesiredVelocity(agentRef.calculateSpeed(targetWorldPos - agentRef.transform.position));
+        //Return the steering force of the desired velocity
+        return agentRef.getDesiredVelocity() - agentRef.getVelocity();
+    }
+
+    /// <summary>
+    /// Returns the steering force for persuing the target
+    /// </summary>
+    /// <param name="target">The agent to seek</param>
+    /// <returns></returns>
+    protected Vector3 pursueSteering(AlienAI target) {
+        //How far ahead (as time) to persue the target
+        float t = Vector3.Distance(target.transform.position, agentRef.transform.position) / (agentRef.getCurrentSpeed() * Time.deltaTime);
+        //Get the future position of the agent
+        Vector3 futurePosition = target.transform.position + (target.getVelocity() * t);
+        //Return the seek steering of the future position
+        return seek(futurePosition);
+    }
+}
