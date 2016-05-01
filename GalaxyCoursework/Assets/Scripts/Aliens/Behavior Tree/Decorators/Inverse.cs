dc4a39bdf81c1f7dc89328c381d5a27e30ebd8a4
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-01 20:40:27 +0100

    Added inverse decorator

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Decorators/Inverse.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/*
+ * Inverse decorator
+ */
+public class Inverse : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+
+    //The child this decorator inverses
+    private Task child;
+
+    //constructor
+    public Inverse(AlienAI agent, Task child) {
+        //Set the agent
+        agentRef = agent;
+        //Set the child
+        this.child = child;
+    }
+
+    public bool activate() {
+        return !child.activate();
+    }
+}
