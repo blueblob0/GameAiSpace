b42edc91d9f6981567d54981f1c8868be3129a68
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-01 20:30:05 +0100

    Added task for not full health

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/HealthNotFull.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Task to check if an agent's health is not full
+ */
+public class HealthNotFull : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public HealthNotFull(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        return agentRef.getHealth() < agentRef.getMaxHealth();
+    }
+}
