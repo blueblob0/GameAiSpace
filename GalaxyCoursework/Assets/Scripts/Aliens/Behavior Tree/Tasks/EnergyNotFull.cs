9e525ce1673b9225f88a77093b1af6d094cabae7
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-01 20:27:07 +0100

    Task for not full energy

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Tasks/EnergyNotFull.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/*
+ * Task to check if an agent's energy is not full
+ */
+public class EnergyNotFull : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public EnergyNotFull(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        return agentRef.getEnergy() < agentRef.getMaxEnergy();
+    }
+}
