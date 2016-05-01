acb37331923c23961532b08b3194b43a105c977f
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-01 20:44:54 +0100

    Added a sequence onto recoup to check for enemies

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Sequences/RecoupSequence.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * The sequence that checks if any enemies are near before doing the recoup selector
+ */
+public class RecoupSequence : Sequence {
+    //The tasks this sequence uses
+    EnemyInSight insight;
+    RecoupSelector recSelect;
+    //The inverse decorator
+    Inverse inv;
+
+    public RecoupSequence(AlienAI agent) : base(agent){
+        //Construct the tasks
+        insight = new EnemyInSight(agent);
+        recSelect = new RecoupSelector(agent);
+        inv = new Inverse(agent, insight);
+
+        //Add the children, order matters
+        addChild(inv);
+        addChild(recSelect);
+    }
+}
