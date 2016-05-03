4ad86491b8ffcf3e23c3948083bb33f208690097
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 15:21:52 +0100

    Added task to check if the agent can reproduce

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Tasks/ReadyToReproduce.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/*
+ * Task to check if the agent can reproduce
+ */
+public class ReadyToReproduce : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public ReadyToReproduce(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        return agentRef.canReproduce();
+    }
+}
