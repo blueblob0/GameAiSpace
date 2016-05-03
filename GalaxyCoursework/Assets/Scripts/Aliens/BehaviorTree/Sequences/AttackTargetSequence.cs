49dedf37fcbeceb2738559a2864ad546e94a8281
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 14:39:26 +0100

    Added sqeuence to attack targets

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Sequences/AttackTargetSequence.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Sequence to check if an enemy is in range and to attack that enemy
+ */
+public class AttackTargetSequence : Sequence {
+    //The tasks for this sequence
+    private EnemyInRange inRange;
+    private Attack attack;
+
+    public AttackTargetSequence(AlienAI agent) : base(agent) {
+        //Construct the sequences
+        inRange = new EnemyInRange(agent);
+        attack = new Attack(agent);
+
+        //Add the tasks, order matters
+        addChild(inRange);
+        addChild(attack);
+    }
+}
