554c77f6e75663ea02baf338f8094d0cdcaaaa60
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 15:40:45 +0100

    Added task to move towards target

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Tasks/MoveToReproductionTarget.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Seeks the reproduction target
+ */
+public class MoveToReproductionTarget : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public MoveToReproductionTarget(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        agentRef.addSteeringForce(seek(agentRef.getReproductionTarget().transform.position));
+        return true;
+    }
+
+    /// <summary>
+    /// Makes the agent seek the target
+    /// </summary>
+    /// <param name="targetWorldPos">The world position of the target to seek</param>
+    /// <returns>Returns the steering force of the desired direction</returns>
+    protected Vector3 seek(Vector3 targetWorldPos) {
+        //Set the diesried velocity to the direction of the target
+        agentRef.setDesiredVelocity(agentRef.calculateSpeed(targetWorldPos - agentRef.transform.position));
+        //Return the steering force of the desired velocity
+        return agentRef.getDesiredVelocity() - agentRef.getVelocity();
+    }
+}
