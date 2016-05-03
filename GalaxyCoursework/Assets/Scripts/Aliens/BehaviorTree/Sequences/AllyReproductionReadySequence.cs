a11fe76fbdbc63edf099236611304efb1259ee5e
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 16:40:37 +0100

    Added sequence to attempt reproduction if an ally is ready

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Sequences/AllyReproductionReadySequence.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Sequence for checking if the ally is ready
+ */
+public class AllyReproductionReadySequence : Sequence {
+    //Tasks for this sequence
+    private AlliesReadyToReproduce ready;
+    private ReproductionTargetInRangeSelector inRange;
+
+    //constructor
+    public AllyReproductionReadySequence(AlienAI agent) : base(agent) {
+        //Construct the tasks in this sequences
+        ready = new AlliesReadyToReproduce(agent);
+        inRange = new ReproductionTargetInRangeSelector(agent);
+
+        //Add children, order is important
+        addChild(ready);
+        addChild(inRange);
+    }
+}
