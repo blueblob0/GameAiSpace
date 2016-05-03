7ab2c17468f1822ee061ad0bd359159502c2bd10
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 15:35:52 +0100

    Added task for checking if the target is in range

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Tasks/ReproductionTargetInRange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/*
+ * Checks if the reproduction target is in range
+ */
+public class ReproductionTargetInRange : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public ReproductionTargetInRange(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        return Vector3.Distance(agentRef.transform.position, agentRef.getReproductionTarget().transform.position) <= 2;
+    }
+
+}
