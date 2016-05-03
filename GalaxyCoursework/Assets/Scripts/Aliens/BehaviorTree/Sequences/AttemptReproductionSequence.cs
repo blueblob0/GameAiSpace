fbc93dc89a65cff77c723596b53ec25601228963
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 16:33:36 +0100

    Added sequence that will reproduce

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Sequences/AttemptReproductionSequence.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Checks if the target is in range, then produces
+ */
+public class AttemptReproductionSequence : Sequence {
+    //Tasks for this sequence
+    private ReproductionTargetInRange inRange;
+    private Reproduce reproduce;
+
+    //constructor
+    public AttemptReproductionSequence(AlienAI agent) : base(agent) {
+        //Construct the tasks
+        inRange = new ReproductionTargetInRange(agent);
+        reproduce = new Reproduce(agent);
+
+        //Add the children, order is important
+        addChild(inRange);
+        addChild(reproduce);
+    }
+}
