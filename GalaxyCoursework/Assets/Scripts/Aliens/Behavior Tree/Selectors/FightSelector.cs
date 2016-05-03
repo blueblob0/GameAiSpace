7faa02e739c2dfd56de5dbc0dd1f02b5f61f9317
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 14:34:56 +0100

    Removed old fight sequence

--- GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Selectors/FightSelector.cs
+++ GalaxyCoursework/Assets/Scripts/Aliens/Behavior Tree/Selectors/FightSelector.cs
@@ -6,20 +6,20 @@ using System.Collections;
  */
 public class FightSelector : Selector {
     //Tasks for this selector
-    private AttackSequence attackSq;
-    private PursueSequence pursueSq;
+    //private AttackSequence attackSq;
+    //private PursueSequence pursueSq;
     private GetTargetSequence getTargSq;
 
     //constructor
     public FightSelector(AlienAI agent) : base (agent) {
         //Construct the tasks
-        attackSq = new AttackSequence(agent);
-        pursueSq = new PursueSequence(agent);
+        //attackSq = new AttackSequence(agent);
+        //pursueSq = new PursueSequence(agent);
         getTargSq = new GetTargetSequence(agent);
 
         //Add the children, order is important
-        addChild(attackSq);
-        addChild(pursueSq);
+        //addChild(attackSq);
+        //addChild(pursueSq);
         addChild(getTargSq);
     }
 }
