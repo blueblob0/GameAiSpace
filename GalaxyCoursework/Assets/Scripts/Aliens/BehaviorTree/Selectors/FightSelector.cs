b2f2dc2a12ad6a244d7e8dbeacefc24005fe9d34
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 14:47:16 +0100

    Updated fight selector to new one

--- GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Selectors/FightSelector.cs
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Selectors/FightSelector.cs
@@ -6,20 +6,17 @@ using System.Collections;
  */
 public class FightSelector : Selector {
     //Tasks for this selector
-    //private AttackSequence attackSq;
-    //private PursueSequence pursueSq;
+    private EngageTargetSequence engage;
     private GetTargetSequence getTargSq;
 
     //constructor
     public FightSelector(AlienAI agent) : base (agent) {
         //Construct the tasks
-        //attackSq = new AttackSequence(agent);
-        //pursueSq = new PursueSequence(agent);
+        engage = new EngageTargetSequence(agent);
         getTargSq = new GetTargetSequence(agent);
 
         //Add the children, order is important
-        //addChild(attackSq);
-        //addChild(pursueSq);
+        addChild(engage);
         addChild(getTargSq);
     }
 }
