8f910330a6e8e7ba2093fa4ddcf0c32c4d35643f
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 16:37:57 +0100

    Added selector to make sure the ally is in range

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Selectors/ReproductionTargetInRangeSelector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Selects the right task if the reproduction target is in range or not
+ */
+public class ReproductionTargetInRangeSelector : Selector {
+    //Tasks for this selector
+    private AttemptReproductionSequence attempt;
+    private MoveToReproductionTarget move;
+
+    //constructor
+    public ReproductionTargetInRangeSelector(AlienAI agent) : base(agent) {
+        //Construct the tasks
+        attempt = new AttemptReproductionSequence(agent);
+        move = new MoveToReproductionTarget(agent);
+
+        //Add the tasks, order is important
+        addChild(attempt);
+        addChild(move);
+    }
+}
