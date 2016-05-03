679ecdb00b54cb296ceca5fc3e529afe63d511aa
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 14:46:16 +0100

    Added sequence to check if a target is present then to engage

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Sequences/EngageTargetSequence.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Sequence to check if a target is present then pursue or attack the target
+ */
+public class EngageTargetSequence : Sequence {
+    //Tasks for this sequence
+    private HaveTarget haveTarg;
+    private PursueAttackSelector purseAttack;
+
+    //construct
+    public EngageTargetSequence(AlienAI agent) : base(agent) {
+        //Construct the tasks
+        haveTarg = new HaveTarget(agent);
+        purseAttack = new PursueAttackSelector(agent);
+
+        //Add the children, order matters
+        addChild(haveTarg);
+        addChild(purseAttack);
+    }
+}
