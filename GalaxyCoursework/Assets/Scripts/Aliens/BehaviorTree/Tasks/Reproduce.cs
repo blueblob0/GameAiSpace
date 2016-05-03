e979d634df5b433f43f3f3617dea57b9d6ba3e92
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 15:38:40 +0100

    Added task to reproduce

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Tasks/Reproduce.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * The Task that will make the agent reproduce
+ */
+public class Reproduce : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public Reproduce(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        agentRef.AttemptReproduction();
+        return true;
+    }
+}
