12cfa3adf6747e745ab283b67e48a2490caf0952
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 16:44:31 +0100

    Sequence for checking if the agent is ready

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Sequences/ReadyToReproduceSequence.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Sequence for checking if the agent is ready
+ */
+public class ReadyToReproduceSequence : Sequence {
+    //Tasks for this sequence
+    private ReadyToReproduce ready;
+    private AllyReproductionReadySequence allyReady;
+
+    //constructor
+    public ReadyToReproduceSequence(AlienAI agent) : base(agent) {
+        //Construct the tasks in this sequences
+        ready = new ReadyToReproduce(agent);
+        allyReady = new AllyReproductionReadySequence(agent);
+
+        //Add children, order is important
+        addChild(ready);
+        addChild(allyReady);
+    }
+}
