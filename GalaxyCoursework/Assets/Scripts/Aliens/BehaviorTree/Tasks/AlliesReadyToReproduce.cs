e7ceda37a1303b63c606844ec57ca8a87fc3583f
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 15:31:35 +0100

    Added Task to get an ally that is ready to repoduce

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Tasks/AlliesReadyToReproduce.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Task checks if there are allies that can reproduce then sets it as the target
+ */
+public class AlliesReadyToReproduce : Task {
+    //Reference to the agent using this behavior
+    protected AlienAI agentRef;
+    //constructor
+    public AlliesReadyToReproduce(AlienAI agent) {
+        agentRef = agent;
+    }
+
+    public bool activate() {
+        if(agentRef.getOtherCreatures().Count <= 0) {
+            return false;
+        } else {
+            foreach(GameObject alien in agentRef.getOtherCreatures()) {
+                if(alien.GetComponent<AlienAI>().canReproduce()) {
+                    agentRef.setReproductionTarget(alien.GetComponent<AlienAI>());
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
