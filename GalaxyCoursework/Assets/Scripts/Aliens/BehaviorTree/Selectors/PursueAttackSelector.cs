437f94fcc2008fa592fc681b676b3e01f3e3ca41
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-03 14:42:28 +0100

    Added selector to pursue or attack

---
+++ GalaxyCoursework/Assets/Scripts/Aliens/BehaviorTree/Selectors/PursueAttackSelector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Selector to pursue or attack agents
+ */
+public class PursueAttackSelector : Selector {
+    //Tasks for this selector
+    private AttackTargetSequence attack;
+    private Pursue pursue;
+
+    //constructor
+    public PursueAttackSelector(AlienAI agent) : base(agent) {
+        //Construct the tasks
+        attack = new AttackTargetSequence(agent);
+        pursue = new Pursue(agent);
+
+        //Add the tasks, order matters
+        addChild(attack);
+        addChild(pursue);
+    }
+}
