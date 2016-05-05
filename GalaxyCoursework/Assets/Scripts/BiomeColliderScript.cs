159a4f2c7a7a47d8d86232618895645a7bee0da6
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-05-05 21:16:38 +0100

    Added basic functionality

--- GalaxyCoursework/Assets/Scripts/BiomeColliderScript.cs
+++ GalaxyCoursework/Assets/Scripts/BiomeColliderScript.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(BoxCollider))]
 public class BiomeColliderScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //Set on spawn
+    [HideInInspector] public Transform parenToSet;
+    [HideInInspector] public biomes biomeType;
+
+    /// <summary>
+    /// Helper function for setting up this collider
+    /// </summary>
+    public void setUp() {
+        StartCoroutine(setParent());
+    }
+
+    /// <summary>
+    /// Sets the parent after 1 frame has passed
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator setParent() {
+        //Wait one frame because of how unity handles instantiating
+        yield return null;
+
+        //now set the parent
+        transform.SetParent(parenToSet);
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        //Let the creature know it is in this biome
+    }
 }
