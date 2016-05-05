0647da760946fff31bd943a33b3d8e93669e4afe
Steven <blueblob0@hotmail.com>
2016-05-05 10:00:17 +0100

    biome

---
+++ GalaxyCoursework/Assets/Scripts/biomes.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum biomes {
+    Land = 0,
+    Forest = 1,
+    Desert = 2,
+    Ice = 3,
+    Water = 4,
+    Mountainous = 5,
+    Lava = 6
+}
